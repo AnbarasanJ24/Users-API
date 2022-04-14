05de22b26418dce910597d222bda0214763e9ef0
Anbarasan <anbarasanj24@gmail.com>
2022-04-14 18:01:47 +0530

    Modified Github Actions

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.users_api = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Go Serverless v2.0! Your function executed successfully!',
         input: event,
       },
       null,
